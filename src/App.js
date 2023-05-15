63769e0a3a69fe5bd77b48a36bab0c5a1f37e6ed
KobiManuel <Emmakobi91@gmail.com>
2023-05-15 12:52:02 +0100

    feat: added scroll to background image

--- src/App.js
+++ src/App.js
@@ -1,10 +1,33 @@
-
-import './App.css';
-import Hero from './Pages/Hero/Main';
+import { useEffect, useRef } from "react";
+import "./App.css";
+import Hero from "./Pages/Hero/Main";
 
 function App() {
+  const containerRef = useRef(null);
+
+  useEffect(() => {
+    const container = containerRef.current;
+
+    const handleMouseMove = (event) => {
+      const { clientX, clientY } = event;
+      const offsetX = (clientX / window.innerWidth) * 20;
+      const offsetY = (clientY / window.innerHeight) * 20;
+
+      container.style.setProperty(
+        "background-position",
+        `calc(100% - ${offsetX}px) calc(50% + ${offsetY}px)`
+      );
+    };
+
+    container.addEventListener("mousemove", handleMouseMove);
+
+    return () => {
+      container.removeEventListener("mousemove", handleMouseMove);
+    };
+  }, []);
+
   return (
-    <div className="App">
+    <div ref={containerRef} className="App">
       <Hero />
       <Hero />
     </div>
@@ -12,4 +35,3 @@ function App() {
 }
 
 export default App;
-//00231a
--- src/Pages/Hero/Main.jsx
+++ src/Pages/Hero/Main.jsx
@@ -1,40 +1,54 @@
-import React, { useEffect, useRef, useState } from 'react'
-import './_Main.scss'
+import React, { useEffect, useRef, useState } from "react";
+import "./_Main.scss";
 
 const Hero = () => {
-    const cardRef = useRef(null);
-    const [isVisible, setIsVisible] = useState(false);
+  const heroRef = useRef(null);
+  const cardRef1 = useRef(null);
+  const cardRef2 = useRef(null);
+  const [isVisible, setIsVisible] = useState(false);
 
-    useEffect(() => {
-      const observer = new IntersectionObserver(
-        ([entry]) => {
-          if (entry.isIntersecting) {
-            setIsVisible(true);
-          }
-        },
-        { threshold: 0.5 }
-      );
+  useEffect(() => {
+    const observer = new IntersectionObserver(
+      ([entry]) => {
+        setIsVisible(entry.isIntersecting);
+      },
+      { threshold: 0.5 }
+    );
 
-      observer.observe(cardRef.current);
+    observer.observe(heroRef.current);
+    observer.observe(cardRef1.current);
+    observer.observe(cardRef2.current);
 
-      return () => observer.unobserve(cardRef.current);
-    }, [isVisible]);
+    return () => {
+      observer.unobserve(heroRef.current);
+      observer.unobserve(cardRef1.current);
+      observer.unobserve(cardRef2.current);
+    };
+  }, [isVisible]);
+
   return (
-    <div
-      className={`project-card ${isVisible ? "visible hero" : "hero"}`}
-      ref={cardRef}
-    >
-      <div>
+    <div className="hero" ref={heroRef}>
+      <div
+        className={`project-card ${
+          isVisible ? "visible" : ""
+        } animate-from-left`}
+        ref={cardRef1}
+      >
         <h3>Kobi</h3>
         <h3>Manuel</h3>
         <span></span>
         <p>Web developer / Visual Artist</p>
       </div>
-      <div>
+      <div
+        className={`project-card ${
+          isVisible ? "visible" : ""
+        } animate-from-right`}
+        ref={cardRef2}
+      >
         <h6>Portfolio</h6>
       </div>
     </div>
   );
-}
+};
 
-export default Hero
+export default Hero;
