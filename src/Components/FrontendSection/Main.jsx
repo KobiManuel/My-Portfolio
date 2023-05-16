a9a5680c76b60210e392bb33b61fd7c219f9eea9
KobiManuel <Emmakobi91@gmail.com>
2023-05-16 12:07:28 +0100

    feat(New Import): imported FrontendSection

--- src/Components/FrontendSection/Main.jsx
+++ src/Components/FrontendSection/Main.jsx
@@ -1,12 +1,19 @@
 import React from 'react'
+import ProjectCard from '../../UI/ProjectCard/ProjectCard';
+import banner from './images/pxfuel.jpg';
 
-const FrontendSection
- = () => {
+const FrontendSection = () => {
   return (
-    <div>FrontendSection
-        
+    <div>
+      <ProjectCard
+        title1="A.I & Developer "
+        title2="Tools"
+        description="Fullstack development"
+        Image={banner}
+        hoverColor="#012c3a"
+      />
     </div>
-  )
+  );
 }
 
 export default FrontendSection
--- src/Pages/MainPage/Main.jsx
+++ src/Pages/MainPage/Main.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import Hero from '../../Components/Hero/Main'
 import DeveloperSection from '../../Components/DeveloperSection/Main'
+import FrontendSection from '../../Components/FrontendSection/Main'
 
 const MainPage = () => {
   return (
     <>
     <Hero />
     <DeveloperSection />
+    <FrontendSection />
     </>
   )
 }
