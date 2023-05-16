c0200e4dd8404089922df0d7abffd5181b684daf
KobiManuel <Emmakobi91@gmail.com>
2023-05-16 12:34:06 +0100

    feat(New Component): created new component

--- src/Components/FrontendSection/Main.jsx
+++ src/Components/FrontendSection/Main.jsx
@@ -1,16 +1,16 @@
 import React from 'react'
 import ProjectCard from '../../UI/ProjectCard/ProjectCard';
-import banner from './images/pxfuel.jpg';
+import banner from './images/banner.jpg';
 
 const FrontendSection = () => {
   return (
     <div>
       <ProjectCard
-        title1="A.I & Developer "
-        title2="Tools"
-        description="Fullstack development"
+        title1="Frontend Dev"
+        title2="Projects"
+        description="Frontend development"
         Image={banner}
-        hoverColor="#012c3a"
+        hoverColor="#012c5a"
       />
     </div>
   );
