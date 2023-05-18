b083c005a91afc61dece9722d3b54316476568f9
KobiManuel <Emmakobi91@gmail.com>
2023-05-18 16:58:16 +0100

    feat: added observer for design page

--- src/Pages/DesignPage/Main.jsx
+++ src/Pages/DesignPage/Main.jsx
@@ -1,8 +1,43 @@
-import React from 'react'
+import React, { useContext, useEffect, useRef } from 'react'
 import banner from './images/banner.jpg';
 import ProjectCard from '../../UI/ProjectCard/ProjectCard';
+import { PortfolioContext } from '../../Context/context';
 
 const DesignPage = () => {
+    const { setThirdCard } = useContext(PortfolioContext);
+    const developerSectionRef = useRef(null);
+
+    useEffect(() => {
+      const observerOptions = {
+        threshold: 0.8, // Set the threshold to 80%
+      };
+
+      const observerCallback = (entries) => {
+        entries.forEach((entry) => {
+          if (entry.isIntersecting) {
+            setThirdCard(true);
+          } else {
+            setThirdCard(false);
+          }
+        });
+      };
+
+      const observer = new IntersectionObserver(
+        observerCallback,
+        observerOptions
+      );
+      const target = developerSectionRef.current;
+
+      if (target) {
+        observer.observe(target);
+      }
+
+      return () => {
+        if (target) {
+          observer.unobserve(target);
+        }
+      };
+    }, [setThirdCard]);
   return (
     <div>
       <ProjectCard
