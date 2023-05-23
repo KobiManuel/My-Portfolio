6ce251cb59d8ebca82f6b3e3d13b631a7951aad1
KobiManuel <Emmakobi91@gmail.com>
2023-05-23 23:52:47 +0100

    fixed

--- src/Pages/MainPage/Main.jsx
+++ src/Pages/MainPage/Main.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import Hero from '../../Components/Hero/Main'
 import DeveloperSection from '../../Components/DeveloperSection/Main'
 import FrontendSection from '../../Components/FrontendSection/Main'
-import DesignPage from '../DesignPage/Main'
+import DesignPage from "../../Components/DesignPage/Main";
 import './_Main.scss';
 import AboutMe from '../../Components/AboutMe/Main'
 import ContactMe from '../../Components/ContactMe/Main'
